b87d9d1cd5598fbb87e1df0dc484f667d5d6a4c3
ng-christina <118376146+ng-christina@users.noreply.github.com>
2023-08-17 20:08:51 +0300

    up

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 import { BASE_URL, API_KEY, SEARCH_PARAMS } from './service/servise';
 import axios from 'axios';
 import Notiflix from 'notiflix';
@@ -9,90 +9,89 @@ import ImageGalleryItem from './ImageGalleryItem/ImageGalleryItem';
 import Loader from './Loader/Loader.';
 import Modal from './Modal/Modal';
 import Searchbar from './Searchbar/Searchbar';
-class App extends Component {
-  state = {
-    name: '',
-    page: 1,
-    hits: [],
-    totalHits: 0,
-    loading: false,
-    showModal: false,
-  };
-  componentDidUpdate(prevProps, prevState) {
-    const { name, page } = this.state;
-    if (prevState.name !== name || prevState.page !== page) {
-      this.getImages();
+const App = () => {
+  const [name, setName] = useState('');
+  const [page, setPage] = useState(1);
+  const [hits, setHits] = useState([]);
+  const [totalHits, setTotalHits] = useState(0);
+  const [loading, setLoading] = useState(false);
+  const [showModal, setShowModal] = useState(false);
+  const [tags, setTags] = useState('');
+  const [largeImageURL, setLargeImageURL] = useState('');
+
+  const fetchImages = useCallback(() => {
+    setLoading(true);
+    axios
+      .get(`${BASE_URL}?key=${API_KEY}&q=${name}&page=${page}&${SEARCH_PARAMS}`)
+      .then(response => {
+        if (!response.data.hits.length) {
+          Notiflix.Notify.failure('No images found!');
+        }
+        const modifiedHits = response.data.hits.map(
+          ({ id, tags, webformatURL, largeImageURL }) => ({
+            id: String(id),
+            tags,
+            webformatURL,
+            largeImageURL,
+          })
+        );
+        setHits(prevHits => [...prevHits, ...modifiedHits]);
+        setTotalHits(response.data.totalHits);
+        setLoading(false);
+      })
+      .catch(error => {
+        console.error(error.message);
+        setLoading(false);
+      });
+  }, [name, page]);
+
+  useEffect(() => {
+    if (name.trim() === '') {
+      return;
     }
-  }
-  getImages = () => {
-    const { name, page } = this.state;
-    this.setState({ loading: true });
-    try {
-      axios
-        .get(
-          `${BASE_URL}?key=${API_KEY}&q=${name}&page=${page}&${SEARCH_PARAMS}`
-        )
-        .then(response => {
-          if (!response.data.hits.length) {
-            Notiflix.Notify.failure('No images found!');
-          }
+    fetchImages();
+  }, [name, page, fetchImages]);
 
-          const modifiedHits = response.data.hits.map(
-            ({ id, tags, webformatURL, largeImageURL }) => ({
-              id,
-              tags,
-              webformatURL,
-              largeImageURL,
-            })
-          );
-          this.setState(prevState => ({
-            hits: [...prevState.hits, ...modifiedHits],
-            totalHits: response.data.totalHits,
-            loading: false,
-          }));
-        });
-    } catch (error) {
-      console.error(error.message);
-      this.setState({ loading: false });
-    }
+  const buttonLoadClick = () => {
+    setPage(prevPage => prevPage + 1);
   };
-  buttonLoadClick = () => {
-    this.setState(prevState => ({ page: prevState.page + 1 }));
-  };
-  handleSubmit = ({ name }) => {
-    this.setState({ hits: [], name, page: 1, totalHits: 0 });
+  const handleSubmit = ({ name }) => {
+    setHits([]);
+    setName(name);
+    setPage(1);
+    setTotalHits(0);
   };
 
-  handleImageClick = (imageURL, tag) => {
-    this.setState({ showModal: true, largeImageURL: imageURL });
+  const handleImageClick = (imageURL, tag) => {
+    setShowModal(prevShowModal => !prevShowModal);
+    setLargeImageURL(imageURL);
+    setTags(tags);
   };
 
-  handleModalClick = () => {
-    this.setState({ showModal: false, largeImageURL: '' });
+  const handleModalClick = () => {
+    setShowModal(false);
+    setLargeImageURL(largeImageURL);
   };
 
-  render() {
-    const { largeImageURL, hits, totalHits, loading, showModal } = this.state;
-    return (
-      <>
-        <Searchbar onSubmit={this.handleSubmit} />
-        {hits.length !== 0 && (
-          <ImageGalleryList>
-            <ImageGalleryItem images={hits} onImage={this.handleImageClick} />
-          </ImageGalleryList>
-        )}
-        {showModal && (
-          <Modal onClose={this.handleModalClick}>
-            <img src={largeImageURL} alt="Modal" />
-          </Modal>
-        )}
-        {loading && <Loader />}
-        {totalHits > 0 && hits.length < totalHits && (
-          <Button onBtnClick={this.buttonLoadClick} />
-        )}
-      </>
-    );
-  }
-}
+  return (
+    <div>
+      <Searchbar onSubmit={handleSubmit} />
+      {hits.length !== 0 && (
+        <ImageGalleryList>
+          <ImageGalleryItem images={hits} onImage={handleImageClick} />
+        </ImageGalleryList>
+      )}
+      {showModal && (
+        <Modal onClose={handleModalClick}>
+          <img src={largeImageURL} alt="Modal" />
+        </Modal>
+      )}
+      {loading && <Loader />}
+      {totalHits > 0 && hits.length < totalHits && (
+        <Button onBtnClick={buttonLoadClick} />
+      )}
+    </div>
+  );
+};
 
 export default App;
